1e8cc59fd6b56c2c4b9d0f84334d619a5fd6f059
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-07-18 12:19:13 +0100

    keep the backend and add the cros config with the angular envirenemnt

---
+++ mon-application-angular/src/app/beneficiaire/beneficiaire.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beneficiaire',
+  standalone: true,
+  imports: [],
+  templateUrl: './beneficiaire.component.html',
+  styleUrl: './beneficiaire.component.css'
+})
+export class BeneficiaireComponent {
+
+}
---
+++ mon-application-angular/src/app/compte-bancaire/compte-bancaire.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compte-bancaire',
+  standalone: true,
+  imports: [],
+  templateUrl: './compte-bancaire.component.html',
+  styleUrl: './compte-bancaire.component.css'
+})
+export class CompteBancaireComponent {
+
+}
